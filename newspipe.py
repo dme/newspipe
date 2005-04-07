583f70e51bbfa764bbfa63b7d8c45b471855ed66
reyesric <reyesric>
2005-04-07 02:49:25 +0000

    Preparing for release 1.1.8

--- newspipe.py
+++ newspipe.py
@@ -2,14 +2,14 @@
 # -*- coding: UTF-8 -*-
 
 # $NoKeywords: $   for Visual Sourcesafe, stop replacing tags
-__revision__ = "$Revision: 1.58 $"
+__revision__ = "$Revision: 1.59 $"
 __revision_number__ = __revision__.split()[1]
-__version__ = "1.1.7"
-__date__ = "2005-03-20"
+__version__ = "1.1.8"
+__date__ = "2005-04-06"
 __url__ = "http://newspipe.sourceforge.net"
 __author__ = "Ricardo M. Reyes <reyesric@ufasta.edu.ar>"
 __contributors__ = ["Rui Carmo <http://the.taoofmac.com/space/>", "Bruno Rodrigues <http://www.litux.org/blog/>"]
-__id__ = "$Id: newspipe.py,v 1.58 2005/04/05 02:21:48 reyesric Exp $"
+__id__ = "$Id: newspipe.py,v 1.59 2005/04/07 02:49:25 reyesric Exp $"
 
 ABOUT_NEWSPIPE = """
 newspipe.py - version %s revision %s, Copyright (C) 2003-%s \n%s
