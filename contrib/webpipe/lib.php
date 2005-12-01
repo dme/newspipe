db88b9ab5cef309ec5bb67df8a7c2e883bc31674
rcarmo <rcarmo>
2005-12-01 18:47:43 +0000

    Added GPL terms, fixed regex.

--- contrib/webpipe/lib.php
+++ contrib/webpipe/lib.php
@@ -1,7 +1,24 @@
 <script language="php">
 // ======================================================================
-// $Id: lib.php,v 1.8 2005/12/01 14:05:34 rcarmo Exp $
-// IMAP Wrapper (non-XSLT version)
+// $Id: lib.php,v 1.9 2005/12/01 18:47:43 rcarmo Exp $
+//
+// WebPipe main library file	 
+//	 
+// Copyright (C) 2005 Rui Carmo, http://the.taoofmac.com	 
+//	 
+// This program is free software; you can redistribute it and/or modify	 
+// it under the terms of the GNU General Public License as published by	 
+// the Free Software Foundation; either version 2 of the License, or	 
+// (at your option) any later version.	 
+//	 
+//  This program is distributed in the hope that it will be useful,	 
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of	 
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the	 
+//  GNU General Public License for more details.	 
+//	 
+//  You should have received a copy of the GNU General Public License	 
+//  along with this program; if not, write to the Free Software	 
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA	 
 // ======================================================================
 
 // constants {{{
@@ -237,7 +254,7 @@ class CIMAPWrapper extends CTemplate { // {{{ IMAP transaction wrapper
         }
       }
     }
-    $szBuffer = preg_replace( '/cid:(.+)"/', "body.php?uid=$szUid&param=" . '$1', $szBuffer );
+    $szBuffer = preg_replace( '/cid:(.+)"/U', "body.php?uid=$szUid&param=" . '$1"', $szBuffer );
     // Remove image sizes from HTML sent to WAP/PDA browsers
     $oProfile = new CProfileManager();
     if( $oProfile->getScreenSize() ) {
